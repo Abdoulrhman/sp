50240adbadedbace0ac0ce019e38b904a399199d
Yassmin911 <ayassmin1998@gmail.com>
2024-10-26 15:56:56 +0300

    Fixing Buiild Issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
 import StudentLogin from "./pages/login";
