bfc552900c928723ed5e812c2e05c59fcf751f30
Abdoulrhman <abdoulrhman.elsaid@smarteducation.ae>
2024-11-03 12:32:34 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://sah-platform-web-ccekg8f2gce3evgt.canadacentral-01.azurewebsites.net/',
+  base: 'https://sah-platform-web-ccekg8f2gce3evgt.canadacentral-01.azurewebsites.net/',
   plugins: [react()],
 })
